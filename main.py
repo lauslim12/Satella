20d818d9959f3d79bb6bdaaec771df74963fc075
lauslim12 <nicholasdwiarto@yahoo.com>
2020-09-30 22:42:16 +0700

    feat: Added weighed random number generation using Inverse CDF math method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Imports
+import math
+from random import randint
+
 # Metadata
 __author__ = "Nicholas Dwiarto Wirasbawa"
 __copyright__ = "Copyright 2020, Nicholas Dwiarto Wirasbawa"
@@ -75,6 +79,27 @@ query ($year: Int, $page: Int, $id: Int, $currentMainCharacterPage: Int, $curren
 class MaxCallsReachedError(Exception):
     def __init__(self):
         self.message = "You have already reached your max calls for this session! Please restart the program!"
+
+
+def generate_weighted_random(max_pages=MAX_ANIME_PER_YEAR_ASSUMPTION):
+    # We want the random number to be 85% biased towards upper numbers (first 10% of total / max pages, ceiled or rounded up).
+    # Method: Random variates with Inverse CDF Method.
+    print("Generating a new biased ID with max randomized number:", max_pages)
+    number = randint(1, 100)
+
+    # If the data is too small (1), we will not use the below function.
+    # Usually, the reason this is used is because we queried by ID (if we search for an anime, the result will always be one page / title only).
+    if max_pages == 1:
+        return 1
+
+    top_anime_pages = math.ceil(max_pages * 0.1)
+
+    print("Top anime pages ranges from page 1 to page {}!".format(top_anime_pages))
+
+    if number <= 85:
+        return randint(1, top_anime_pages)
+    else:
+        return randint(top_anime_pages + 1, max_pages)
 
 
 def check_if_already_at_the_limit(data):
