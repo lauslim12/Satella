4b2ac3d0bd5b35e042ab3e018f498ccc0bef688e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-06-20 17:08:07 +0700

    feat: Add helpers for Genderize API

---
+++ src/genderize.py
@@ -0,0 +1,33 @@
+"""API helpers to help with Genderize API."""
+
+import asyncio
+
+import aiohttp
+from aiohttp.client import ClientSession
+
+from constants import GENDERIZE_API_URL
+from datatypes import GenderizeResponse
+
+
+async def fetch_genderize(session: ClientSession, parameter: str) -> GenderizeResponse:
+    """Fetches data asynchronously from Genderize API."""
+    formatted_url = f"{GENDERIZE_API_URL}/{parameter}"
+
+    async with session.get(formatted_url) as response:
+        return await response.json()
+
+
+async def fetch_genders_from_genderize(
+    parameters: list[str],
+) -> list[GenderizeResponse]:
+    """Prepare to do several parallel requests to the Genderize API."""
+    tasks = []
+
+    async with aiohttp.ClientSession() as session:
+        for parameter in parameters:
+            task = asyncio.create_task(fetch_genderize(session, parameter))
+            tasks.append(task)
+
+        # alternative to 'Promise.all' in JavaScript
+        responses: list[GenderizeResponse] = await asyncio.gather(*tasks)
+        return responses
